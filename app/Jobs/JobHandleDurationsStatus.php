3807b31b76a59805b016a068c7c88d96f1ed7a7d
rolmo <rodrigolmo.dev@gmail.com>
2024-07-24 17:14:33 -0300

    Job handle status durations done

---
+++ app/Jobs/JobHandleDurationsStatus.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Duration;
+use App\Models\User;
+use App\Models\Task;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use Illuminate\Support\Facades\Log;
+
+use Carbon\Carbon;
+
+class JobHandleDurationsStatus implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+
+        $now = Carbon::now('America/Sao_Paulo');
+
+        Log::info($now);
+
+        $tasks = Task::all();
+
+        foreach ($tasks as $task) {
+
+            $isRecurrentTask = $task->reminder->recurring->specific_date == null;
+
+            if ($isRecurrentTask) {
+
+                foreach ($task->durations as $duration) {
+
+                    $start =  getCarbonTime($duration->start);
+                    $end =  getCarbonTime($duration->end);
+
+                    $isInProgress = $start->lessThanOrEqualTo($now) && $end->greaterThanOrEqualTo($now);
+                    $isFinished = $end->lessThan($now);
+                    $isStarting = $start->greaterThan($now);
+
+                    if ($isInProgress) {
+
+                        $duration->update(['status' => 'in_progress']);
+                    } elseif ($isFinished) {
+
+                        $duration->update(['status' => 'finished']);
+                    } elseif ($isStarting) {
+
+                        $duration->update(['status' => 'starting']);
+                    }
+                }
+            } else {
+
+                foreach ($task->durations as $duration) {
+
+                    $start =  getCarbonTime($duration->start);
+                    $end =  getCarbonTime($duration->end);
+
+                    $isInProgress = ($start->lessThanOrEqualTo($now) && $end->greaterThanOrEqualTo($now)) && $duration->stauts == 'starting';
+                    $isFinished = $end->lessThan($now) && $duration->stauts == 'starting';
+
+                    if ($isInProgress) {
+                        $duration->update(['status' => 'in_progress']);
+                    } elseif ($end->lessThan($now)) {
+
+                        $duration->update(['status' => 'finished']);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/Jobs/JobSendInvitationEmail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Mail\TaskInvitationMail;
+
+use Mail;
+
+class JobSendInvitationEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+
+    protected $participantsEmails;
+    protected $task;
+    protected $creatorName;
+
+    public function __construct($participantsEmails, $task, $creatorName)
+    {
+        $this->participantsEmails = $participantsEmails;
+        $this->task = $task;
+        $this->creatorName = $creatorName;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Mail::to($this->participantsEmails)->send(new TaskInvitationMail($this->task, $this->creatorName));
+    }
+}
