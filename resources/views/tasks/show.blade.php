96b4b81a40711ea04d08d4484eaf6e0915102287
rolmo <rodrigolmo.dev@gmail.com>
2024-09-09 16:06:41 -0300

    minor changes

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -40,11 +40,22 @@
 
                                 </div>
 
-                                <div class="col-md-2 d-flex align-items-end justify-content-end">
-                                    <button class="btn btn-primary" type="button" data-bs-toggle="collapse"
+                                {{-- Botão de visualizar detalhes --}}
+                                <div class="col-md-2 d-flex align-items-start justify-content-end">
+
+                                    <button class="btn btn-secondary shadow" type="button" data-bs-toggle="collapse"
                                         data-bs-target="#collapseDetails" aria-expanded="false"
-                                        aria-controls="collapseDetails">
-                                        Detalhes
+                                        aria-controls="collapseDetails" title="Visualizar os detalhes da tarefa">
+
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
+                                            viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                        </svg>
+
                                     </button>
                                 </div>
 
@@ -173,8 +184,8 @@
                 <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown"
                     aria-expanded="false">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="32" height="32" viewBox="0 0 24 24"
-                        stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" width="32" height="32"
+                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                     </svg>
